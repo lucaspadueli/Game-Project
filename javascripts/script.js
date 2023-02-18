c538bc25738c3a4adfbc8ee7c2b5104e2e815fb1
lucas.padueli <lucascapellapadueli@gmail.com>
2023-02-18 20:54:40 -0300

    organizacao do codigo

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,20 +1,31 @@
-const canvas = document.getElementById("canvas-project")
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas-project");
+const ctx = canvas.getContext("2d");
 
 const firstScreen = document.getElementById("first-screen");
 const ctx3 = firstScreen.getContext("2d");
 const youWon = document.getElementById("you-won");
 const ctx4 = youWon.getContext("2d");
+let gameStarted = false;
+let time = 0;
+let gameOver = false;
+let score = 0;
+let facesArray = [];
+let intervalId = null;
 startScreen();
 
-let gameStarted = false;
+function timer() {
+  time += 1;
+  console.log(`Time is ${time}`);
+  if (time >= 60) {
+    clearInterval(intervalId);
+  }
+}
 
 
-
-function startScreen () {
+function startScreen() {
   ctx3.fillStyle = "blue";
   ctx3.fillRect(0, 0, 700, 650);
-  
+
   // draw the button
   ctx3.fillStyle = "red";
   ctx3.fillRect(250, 300, 200, 100);
@@ -29,15 +40,15 @@ function startScreen () {
 
     if (x >= 250 && x <= 450 && y >= 300 && y <= 400) {
       gameStarted = true;
-      if(gameStarted == true){
+      
       document.body.appendChild(canvas);
       document.body.removeChild(firstScreen);
       document.body.removeChild(youWon);
       canvas.style.display = "block";
-      updateCanvas();
-      time = 0;
+      
+      
       document.body.removeChild(gameOverCanvas);
-      document.addEventListener('keydown', e => {
+      document.addEventListener("keydown", (e) => {
         console.log(e);
         switch (e.keyCode) {
           case 38:
@@ -52,148 +63,138 @@ function startScreen () {
           case 39:
             hero.moveRight();
             break;
-            case 32:
-              hero.slideRight();
-              break;
-              case 86:
-                hero.slideLeft();
-                break;
-                
+          case 32:
+            hero.slideRight();
+            break;
+          case 86:
+            hero.slideLeft();
+            break;
         }
-        updateCanvas();
-      })
-
-    } }
+        
+      });
+      requestAnimationFrame(updateCanvas);
+      intervalId = setInterval(timer,1000);
+    }
   });
 }
 
-startScreen();
-
 class Hero {
-  constructor(){
+  constructor() {
     this.x = 120;
     this.y = 450;
     this.jumpTimeout = null;
-    const img = new Image ();
+    const img = new Image();
     img.addEventListener("load", () => {
       this.img = img;
-
-    })
-    img.src = "https://www.avatarsinpixels.com/minipix/eyJXaW5ncyI6IjYiLCJIYWlyTG93ZXIiOiIzIiwiQ2FwZUJhY2siOiIxIiwiQm9keSI6IjIiLCJFeWVzIjoiMSIsIk1vdXRoIjoiMSIsIlNvY2tzIjoiMTMiLCJTaG9lcyI6IjMiLCJHbG92ZXMiOiIyIiwiUGFudHMiOiIxIiwiVG9wIjoiNiIsIkphY2tldCI6IjMiLCJDYXBlIjoiNSIsIkhhaXIiOiIyMyIsImV5ZXNUb25lIjoiZmI0ODM1IiwiZXllc1RvbmUyIjoiZGFjNjkwIiwibWFza1RvbmUiOiJiZDg5NGIiLCJoYWlyVG9uZSI6IjkyYTBjYyIsImhhaXJUb25lMiI6ImNmZTNkNCIsInVuZGVyd2VhclRvbmUiOiJmMjdiMjkiLCJ1bmRlcndlYXJUb25lMiI6ImFmNWZiNSIsInBhbnRzVG9uZSI6IjA2MjlmNyIsInBhbnRzVG9uZTIiOiJjNzcxZjciLCJ0b3BUb25lIjoiZDFmNjhhIiwidG9wVG9uZTIiOiJlYWJlYzQiLCJ3aW5nc1RvbmUiOiJlZDUzNmYiLCJ3aW5nc1RvbmUyIjoiNTYwYTE5Iiwic2hvZXNUb25lIjoiN2M2YTcwIiwic29ja3NUb25lIjoiYjg5ZjYyIiwic29ja3NUb25lMiI6IjhlZTU3YiIsImdsb3Zlc1RvbmUiOiJmZmE3YWIiLCJnbG92ZXNUb25lMiI6IjdhMGM0MCIsImhhdFRvbmUiOiJmZGRlYTMiLCJoYXRUb25lMiI6IjQyN2RjYSIsImNhcGVUb25lIjoiMjFkYTA2IiwiY2FwZVRvbmUyIjoiZjNiNTI0IiwiYmVsdFRvbmUiOiIwZmMyYzciLCJqYWNrZXRUb25lIjoiNjA1NDllIiwiamFja2V0VG9uZTIiOiJkMzlmZWYiLCJuZWNrVG9uZSI6ImJkYmVlZCIsIm5lY2tUb25lMiI6ImYxNzQ5NSJ9/1/show.png";
+    });
+    img.src =
+      "https://www.avatarsinpixels.com/minipix/eyJXaW5ncyI6IjYiLCJIYWlyTG93ZXIiOiIzIiwiQ2FwZUJhY2siOiIxIiwiQm9keSI6IjIiLCJFeWVzIjoiMSIsIk1vdXRoIjoiMSIsIlNvY2tzIjoiMTMiLCJTaG9lcyI6IjMiLCJHbG92ZXMiOiIyIiwiUGFudHMiOiIxIiwiVG9wIjoiNiIsIkphY2tldCI6IjMiLCJDYXBlIjoiNSIsIkhhaXIiOiIyMyIsImV5ZXNUb25lIjoiZmI0ODM1IiwiZXllc1RvbmUyIjoiZGFjNjkwIiwibWFza1RvbmUiOiJiZDg5NGIiLCJoYWlyVG9uZSI6IjkyYTBjYyIsImhhaXJUb25lMiI6ImNmZTNkNCIsInVuZGVyd2VhclRvbmUiOiJmMjdiMjkiLCJ1bmRlcndlYXJUb25lMiI6ImFmNWZiNSIsInBhbnRzVG9uZSI6IjA2MjlmNyIsInBhbnRzVG9uZTIiOiJjNzcxZjciLCJ0b3BUb25lIjoiZDFmNjhhIiwidG9wVG9uZTIiOiJlYWJlYzQiLCJ3aW5nc1RvbmUiOiJlZDUzNmYiLCJ3aW5nc1RvbmUyIjoiNTYwYTE5Iiwic2hvZXNUb25lIjoiN2M2YTcwIiwic29ja3NUb25lIjoiYjg5ZjYyIiwic29ja3NUb25lMiI6IjhlZTU3YiIsImdsb3Zlc1RvbmUiOiJmZmE3YWIiLCJnbG92ZXNUb25lMiI6IjdhMGM0MCIsImhhdFRvbmUiOiJmZGRlYTMiLCJoYXRUb25lMiI6IjQyN2RjYSIsImNhcGVUb25lIjoiMjFkYTA2IiwiY2FwZVRvbmUyIjoiZjNiNTI0IiwiYmVsdFRvbmUiOiIwZmMyYzciLCJqYWNrZXRUb25lIjoiNjA1NDllIiwiamFja2V0VG9uZTIiOiJkMzlmZWYiLCJuZWNrVG9uZSI6ImJkYmVlZCIsIm5lY2tUb25lMiI6ImYxNzQ5NSJ9/1/show.png";
   }
   moveUp() {
-  if (this.jumpTimeout) return;
-  let jumpInterval = setInterval(() => {
-    this.y -= 9;
-    if (this.y <= 500 - 170) {
-      clearInterval(jumpInterval);
-      let fallInterval = setInterval(() => {
-        this.y += 9;
-        if (this.y >= 500 - 50) {
-          clearInterval(fallInterval);
-          this.jumpTimeout = setTimeout(() => {
-            this.jumpTimeout = null;
-          }, 300);
-        }
-      }, 10);
-    }
-  }, 5);
-}
+    if (this.jumpTimeout) return;
+    let jumpInterval = setInterval(() => {
+      this.y -= 9;
+      if (this.y <= 500 - 170) {
+        clearInterval(jumpInterval);
+        let fallInterval = setInterval(() => {
+          this.y += 9;
+          if (this.y >= 500 - 50) {
+            clearInterval(fallInterval);
+            this.jumpTimeout = setTimeout(() => {
+              this.jumpTimeout = null;
+            }, 300);
+          }
+        }, 10);
+      }
+    }, 5);
+  }
   moveDown() {
-    if(this.y < 450){
-      this.y += 25};
+    if (this.y < 450) {
+      this.y += 25;
+    }
   }
   moveLeft() {
-    if(this.x > 20){
-    this.x -= 48};
+    if (this.x > 20) {
+      this.x -= 48;
+    }
   }
   moveRight() {
-    if(this.x < 603){
-    this.x += 48};
+    if (this.x < 603) {
+      this.x += 48;
+    }
   }
 
-  slideRight(){
+  slideRight() {
     this.x += 170;
   }
-  slideLeft(){
+  slideLeft() {
     this.x -= 170;
   }
   draw() {
     ctx.drawImage(this.img, this.x, this.y, 70, 70);
-    
   }
 }
 
 class Floor {
-  constructor () {
-   this.x = 0;
-   this.y = 518;
-   this.width = 700;
-   this.height = 150;
-   this.color = "green";
+  constructor() {
+    this.x = 0;
+    this.y = 518;
+    this.width = 700;
+    this.height = 150;
+    this.color = "green";
   }
 
   draw() {
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x,this.y,this.width,this.height);
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 
-  update (){
-  
-  
-}}
-
+  update() {}
+}
 
 class DarkSadFace {
-  constructor(x,y){
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.speed = 5;
-    const img = new Image ();
+    const img = new Image();
     img.addEventListener("load", () => {
       this.img = img;
-
-    })
+    });
     img.src = "/images/sad-face.png";
   }
-  
+
   draw() {
     ctx.drawImage(this.img, this.x, this.y, 70, 70);
   }
- 
+
   update() {
     this.y += this.speed;
-    if(time == 0){
+    if (time == 0) {
       this.x = 50;
       this.y = 0;
-    }
-    else if (this.y > 650) {
+    } else if (this.y > 650) {
       this.y = 0;
-      let darkPositions = [120,160,240,300,360,400,480,550,620];
-      for(let i = 0; i < darkPositions.length; i++){
-        this.x = darkPositions[Math.floor(Math.random() * darkPositions.length)]
+      let darkPositions = [120, 160, 240, 300, 360, 400, 480, 550, 620];
+      for (let i = 0; i < darkPositions.length; i++) {
+        this.x =
+          darkPositions[Math.floor(Math.random() * darkPositions.length)];
       }
     }
-    
   }
-
 }
 
 // happy yellow face.
 
 class HappyYellowFace {
-  constructor (x,y,speed) {
+  constructor(x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = 5;
     this.collisionDetected = false;
-    
   }
 
-  draw (ctx) {
-   
-     
-    
+  draw(ctx) {
     ctx.fillStyle = "yellow";
     ctx.beginPath();
     ctx.arc(this.x, this.y, 25, 0, 2 * Math.PI);
@@ -219,26 +220,22 @@ class HappyYellowFace {
     ctx.beginPath();
     ctx.arc(this.x, this.y - 8, 4, Math.PI, 2 * Math.PI, false);
     ctx.stroke();
-   }
+  }
   update() {
-    
     this.y += this.speed;
     if (this.y > 650) {
       this.y = 0;
-      let positionsX = [50,100,140,200,240,290,350,400,450,480,500,530,570];
-      for(let i = 0; i < positionsX.length; i++){
-        this.x = positionsX[Math.floor(Math.random() * 13)]
+      let positionsX = [
+        50, 100, 140, 200, 240, 290, 350, 400, 450, 480, 500, 530, 570,
+      ];
+      for (let i = 0; i < positionsX.length; i++) {
+        this.x = positionsX[Math.floor(Math.random() * 13)];
       }
       this.collisionDetected = false;
-      
+    } else if (time == 0) {
+      this.y = 0;
     }
-    else if(time == 0){
-      this.y = 0 
-    }
-   
-    
   }
-
 }
 
 class Flower {
@@ -259,77 +256,58 @@ class Flower {
     ctx.arc(this.x, this.y, 15, 0, 2 * Math.PI);
     ctx.fill();
     for (let i = 0; i < 360; i += 45) {
-      let x = this.x + Math.cos(i * Math.PI / 180) * 15;
-      let y = this.y + Math.sin(i * Math.PI / 180) * 15;
+      let x = this.x + Math.cos((i * Math.PI) / 180) * 15;
+      let y = this.y + Math.sin((i * Math.PI) / 180) * 15;
       ctx.fillStyle = "yellow";
       ctx.beginPath();
       ctx.arc(x, y, 5, 0, 2 * Math.PI);
       ctx.fill();
     }
-    
-    
   }
 
-  update() {
-    this.y -= this.speed;
-    if (time == 0){
-      this.y = 650;
-      this.x = 350;
-    }
+update() {
+  this.y -= this.speed;
+  if (time == 0) {
+  this.y = 650;
+  this.x = 350;
+                 } 
     else if (this.y <= 0) {
       this.y = 650;
-      this.x = Math.floor(Math.random() * 700)
-    }
+      this.x = Math.floor(Math.random() * 700);
+                          }
     this.collisionDetected = false;
   }
 }
 
-
-let darkSadFace = new DarkSadFace(400,0,5);
-let happyYellowFace = new HappyYellowFace(Math.floor(Math.random() * 700),0,5);
+let darkSadFace = new DarkSadFace(400, 0, 5);
+let happyYellowFace = new HappyYellowFace(
+Math.floor(Math.random() * 700),0,5);
 const floor = new Floor();
 const hero = new Hero();
-let flower = new Flower (300, 650, 3);
-let secondFace = new HappyYellowFace (Math.floor(Math.random() * 700),0);
+let flower = new Flower(300, 650, 3);
+let secondFace = new HappyYellowFace(Math.floor(Math.random() * 700), 0);
+let happyArray = [];
 
-
-let intervalId = setInterval(() => {
-  darkSadFace.update();
-  happyYellowFace.update();
-  flower.update();
-  floor.update();
-  secondFace.update(); 
-  
-}, 16);
-
-
-
-
-
+happyArray.push(new HappyYellowFace());
 
 const sky = new Image();
 sky.src = "/images/starPic.png";
-sky.addEventListener("load", ()=> {
+sky.addEventListener("load", () => {
   updateCanvas();
-})
+});
 
-let time = 0;
 
-function timer (){
-time += 1
-console.log(`Time is ${time}`);
-if(time >= 60){
-  clearInterval(intervalId)
-}
-}
-const timerInterval = setInterval(timer,1000);
-timer()
-  
+// const timerInterval = setInterval(timer, 1000);
+
+
 function collision(hero, face) {
-  if (hero.x < face.x + 60 &&
-      hero.x + 60 > face.x &&
-      hero.y < face.y + 60 &&
-      hero.y + 60 > face.y && !face.collisionDetected) {
+  if (
+    hero.x < face.x + 60 &&
+    hero.x + 60 > face.x &&
+    hero.y < face.y + 60 &&
+    hero.y + 60 > face.y &&
+    !face.collisionDetected
+  ) {
     face.collisionDetected = true;
     face.y = 700;
     return 5;
@@ -338,60 +316,72 @@ function collision(hero, face) {
   }
 }
 
-function secondCollision (hero,face){
-  if (hero.x < face.x + 50 &&
+function secondCollision(hero, face) {
+  if (
+    hero.x < face.x + 50 &&
     hero.x + 50 > face.x &&
     hero.y < face.y + 50 &&
-    hero.y + 50 > face.y && !face.collisionDetected) {
-  face.collisionDetected = true;
-  face.y = 700;
-  return 5;
-} else {
-  return 0;
-}
+    hero.y + 50 > face.y &&
+    !face.collisionDetected
+  ) {
+    face.collisionDetected = true;
+    face.y = 700;
+    return 5;
+  } else {
+    return 0;
+  }
 }
 
-function flowerCollision (hero, flower){
-  if (hero.x < flower.x + 50 &&
+function flowerCollision(hero, flower) {
+  if (
+    hero.x < flower.x + 50 &&
     hero.x + 50 > flower.x &&
     hero.y < flower.y + 50 &&
-    hero.y + 50 > flower.y && !flower.collisionDetected) {
-  flower.collisionDetected = true;
-  flower.y = 0;
-  return -5;
-} else {
-  return 0;
-}
+    hero.y + 50 > flower.y &&
+    !flower.collisionDetected
+  ) {
+    flower.collisionDetected = true;
+    flower.y = 0;
+    return -5;
+  } else {
+    return 0;
+  }
 }
 
-function darkCollision (hero, face){
-  if (hero.x < face.x + 40 &&
+function darkCollision(hero, face) {
+  if (
+    hero.x < face.x + 40 &&
     hero.x + 40 > face.x &&
     hero.y < face.y + 40 &&
-    hero.y + 40 > face.y && !face.collisionDetected) {
-     return true;
-  face.y = 700;
-  
-} }
+    hero.y + 40 > face.y &&
+    !face.collisionDetected
+  ) {
+    return true;
+    face.y = 700;
+  }
+}
 
-function secondDarkCollision (hero, face){
-  if (hero.x < face.x + 50 &&
+function secondDarkCollision(hero, face) {
+  if (
+    hero.x < face.x + 50 &&
     hero.x + 50 > face.x &&
     hero.y < face.y + 50 &&
-    hero.y + 50 > face.y && !face.collisionDetected) {
-     return true;
-  face.y = 700;
-  
-} }
-let gameOver = false;
+    hero.y + 50 > face.y &&
+    !face.collisionDetected
+  ) {
+    return true;
+    face.y = 700;
+  }
+}
 
 
 function updateCanvas() {
-  
- if(time > 5){
-  secondFace.draw(ctx)
- }
-  ctx.clearRect(0, 0, 700, 650)
+  darkSadFace.update();
+  happyYellowFace.update();
+  flower.update();
+  floor.update();
+  secondFace.update();
+  ctx.clearRect(0, 0, 700, 650);
   ctx.drawImage(sky, 0, 0, 700, 650);
   floor.draw();
   hero.draw();
@@ -400,55 +390,47 @@ function updateCanvas() {
   flower.draw(ctx);
   ctx.font = "20px Arial";
   ctx.fillStyle = "white";
-  ctx.fillText(`Time: ${time}`,10, 25);
-  ctx.font = "20px Arial";
-  ctx.fillStyle = "white";
-  score += collision(hero,happyYellowFace);
-  score += secondCollision(hero,secondFace);
-  score += flowerCollision(hero,flower);
-  if(darkCollision (hero,darkSadFace)){
+  ctx.fillText(`Time: ${time}`, 10, 25);
+  
+  score += collision(hero, happyYellowFace);
+  score += secondCollision(hero, secondFace);
+  score += flowerCollision(hero, flower);
+  if (darkCollision(hero, darkSadFace)) {
     gameOver = true;
     canvas.style.display = "none";
     gameOverCanvas.style.display = "block";
     drawGameOver(score);
-  } 
-  
-  
-  ctx.fillText(`Score: ${score}`,600, 25);
-  if(time > 5 && time < 30){
-  
+  }
+
+  ctx.fillText(`Score: ${score}`, 600, 25);
+  if (time > 20 && time < 30) {
     happyYellowFace.speed = 7;
     darkSadFace.speed = 7;
     flower.speed = 5;
-  
-  }
-  else if(time >= 30 && time <40){
+  } else if (time >= 30 && time < 40) {
     happyYellowFace.speed = 9;
     darkSadFace.speed = 9;
     flower.speed = 7;
-  }
-  else if(time > 40 && time <50){
+  } else if (time > 40 && time < 50) {
     happyYellowFace.speed = 11;
     darkSadFace.speed = 11;
     flower.speed = 7;
-  }
-  else if(time > 50){
+  } else if (time > 50) {
     happyYellowFace.speed = 13;
     darkSadFace.speed = 13;
     flower.speed = 7;
   }
 
-
-  if(!gameOver){
-    requestAnimationFrame(updateCanvas)
-  }
-  else if(gameOver){
-    
-    document.body.appendChild(gameOverCanvas)
+  if (!gameOver) {
+    requestAnimationFrame(updateCanvas);
+  } else if (gameOver) {
+    document.body.appendChild(gameOverCanvas);
+    document.body.removeChild(canvas)
+    clearInterval(intervalId);
   }
   function drawGameOver(score) {
     ctx2.fillStyle = "black";
-    ctx2.fillRect(0,0,700,650);
+    ctx2.fillRect(0, 0, 700, 650);
     ctx2.font = "30px Arial";
     ctx2.fillStyle = "white";
     ctx2.fillText(`Game-Over! You scored ${score} points`, 100, 200);
@@ -456,34 +438,32 @@ function updateCanvas() {
     ctx2.fillStyle = "white";
     ctx2.fillText(`Wanna try again?`, 200, 300);
   }
-  function drawYouWon (score){
+  function drawYouWon(score) {
     ctx4.fillStyle = "black";
-    ctx4.fillRect(0,0,700,650);
+    ctx4.fillRect(0, 0, 700, 650);
     ctx4.font = "20px Arial";
     ctx4.fillStyle = "white";
-    ctx4.fillText(`Congratulation! You won! You scored ${score} points`, 100, 200);
+    ctx4.fillText(
+      `Congratulation! You won! You scored ${score} points`,
+      100,
+      200
+    );
   }
-  if(time >= 60 && score >= 90){
+  if (time >= 60 && score >= 90) {
     gameOver = true;
     canvas.style.display = "none";
     gameOverCanvas.style.display = "none";
     document.body.appendChild(youWon);
     drawYouWon(score);
-  } 
-  else if(time >= 60 && score < 90){
+  } else if (time >= 60 && score < 90) {
     gameOver = true;
     canvas.style.display = "none";
     gameOverCanvas.style.display = "block";
     drawGameOver(score);
   }
 }
-let score = 0;
-requestAnimationFrame(updateCanvas);
+
 
 
 const gameOverCanvas = document.getElementById("game-over-canvas");
 const ctx2 = gameOverCanvas.getContext("2d");
-
-
-
-
